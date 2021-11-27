24a84258e1630bc77a8af8d23ea69fcab4e0e411
DinoTheDev <77805841+DinoTheDevOfficial@users.noreply.github.com>
2021-11-27 16:13:04 -0700

    Update heart.js

--- heart.js
+++ heart.js
@@ -2,7 +2,18 @@ global.config = require("./config.json")
 const aoijs = require("aoi.js")
 const bot = new aoijs.Bot({
   token: config.TOKEN, 
-  prefix: "C!", 
-  intents: "all"
+  prefix: "C!",
+  intents: "all",
+  database: {
+    type : "custom",
+    db : require('quick.db'), // dont modify unless you want a new db provider
+    tables : ['main'], // add more if you want
+    promisify : true
+  } // delete this if you want json db and not sqlite db
 })
 bot.onMessage()
+
+
+bot.variables({
+  "prefix": "C!"
+}, 'main')
